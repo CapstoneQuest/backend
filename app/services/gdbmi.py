441921e8abe804642a6d660b931e388e7051f524
thebenzo <amanvrma5799@gmail.com>
2024-10-07 00:26:27 +0530

    Refactor stack and heap update function

--- app/services/gdbmi.py
+++ app/services/gdbmi.py
@@ -7,17 +7,21 @@ def get_trace_step(gdb_controller, gdb_command):
     
     stdout = next((res['payload'] for res in results if res['type'] == 'output'), '')
     
+    heap = {}
+    stack_frames = []
+    update_program_state(gdb_controller=gdb_controller, stack_frames=stack_frames, heap=heap)
+
     step.update({'function': results[-1]['payload']['frame']['func'],
                  'line': results[-1]['payload']['frame']['line'],
-                 'stack_frames': get_stack_frames(gdb_controller=gdb_controller),
+                 'stack_frames': stack_frames,
+                 'heap': heap,
                  'stdout': stdout})
     return step
 
-def get_stack_frames(gdb_controller):
+def update_program_state(gdb_controller, stack_frames, heap):
     results = gdb_controller.write('-stack-list-frames')
     stack = results[0]['payload']['stack']
-    
-    stack_frames = []
+
     for frame in stack:
         stack_frame = {'function': frame['func'], 'local_variables': []}
         frame_level = frame['level']
@@ -29,17 +33,16 @@ def get_stack_frames(gdb_controller):
         local_vars = results[0]['payload']['variables']
         for var in local_vars:
             primitive_var = get_primitive_variable(gdb_controller=gdb_controller, primitive_variable=var)
-            local_variables.append(primitive_var)
 
+            local_variables.append(primitive_var)
+        
         stack_frame.update({'local_variables': local_variables})
         stack_frames.append(stack_frame)
-
-    return stack_frames
 
 def get_primitive_variable(gdb_controller, primitive_variable):
     var_name = primitive_variable['name']
     var_dtype = primitive_variable['type']
-    var_value = primitive_variable['value']
+    var_value = primitive_variable['value'].split(' ')[0]
     results = gdb_controller.write(f'-data-evaluate-expression &{var_name}')
     var_address = results[0]['payload']['value']
 
