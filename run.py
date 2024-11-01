3a00ba601a5421d3616d0029696b888f261579eb
thebenzo <amanvrma5799@gmail.com>
2024-11-02 01:12:34 +0530

    Change flask app creation and add CORS support

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 from app import create_app
 from config import Config
-from flask import g
 
 app = create_app(Config)
-
-@app.before_request
-def before_request():
-    g.variables_dict = {}
 
 if __name__ == '__main__':
     app.run(debug=True)
